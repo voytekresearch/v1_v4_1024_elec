e97c5b23537cc33a170e6321c39cd589565684f1
andyrdar <aruizdargence@ucsd.edu>
2022-11-23 11:44:24 -0800

    create script for all complete sessions

--- scripts/fit_psd.py
+++ scripts/fit_psd.py
@@ -11,7 +11,7 @@ from fooof import FOOOFGroup
 FS = 500 # sampling frequency (Hz)
 N_ARRAYS = 16 # number of arrays
 N_CHANNELS = 64 # number of channels per array
-SESSIONS = 'A_SNR_041018'
+SESSIONS = 'A_SNR_140819'
 
 # settings
 PATH = 'G:/Shared drives/v1_v4_1024/'
