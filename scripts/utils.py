6c98fa01ba46ece3b7f6f1ae2ecabda32b05268c
Michael <mpreston@ucsd.edu>
2023-01-09 13:50:44 -0800

    add utils.py funcs to epoch LFP

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,3 +6,61 @@ Utility functions
 # general imports
 import numpy as np
 
+def epoch_neo_segment(segment, reset_time=True):
+    """Split a Neo segment into epochs and reshape to 3D array.
+
+    Parameters
+    ----------
+    segment : neo.Segment
+        Neo segment object to be epoched. Must contain epochs.
+    reset_time : bool, optional
+        Whether to reset the time of the epochs to 0. The default is True.
+    Returns
+    -------
+    signal_epoch : np.ndarray
+        Matrix of epochs with shape (trials x channels x time).
+    """
+
+    # imports
+    from neo.utils import cut_segment_by_epoch
+    from copy import copy
+
+    # use Neo utils to epoch
+    seg_lst = cut_segment_by_epoch(copy(segment), segment.epochs[0], reset_time=reset_time)
+
+    # Reformat to array
+    signal_epoch = []
+    for seg_i in seg_lst:
+        signal_epoch.append(seg_i.analogsignals[0].magnitude)
+
+    # Reshape (trials x channels x time)
+    signal_epoch = np.dstack(signal_epoch)
+    signal_epoch = np.transpose(signal_epoch, axes=[2,1,0])
+
+    return signal_epoch
+
+def epoch_nix(fname):
+    """
+    Epochs a segment from a NIX file.
+
+    Parameters
+    ----------
+    fname : str
+        File name of the NIX file.
+
+    Returns
+    -------
+    signal_epoch: array
+        Array of epochs of shape (trials x channels x time).
+    """
+
+    # imports
+    from io import load_nix
+    
+    # Load nix
+    segment, _ = load_nix(fname)
+
+    # epoch
+    signal_epoch = epoch_neo_segment(segment)
+
+    return signal_epoch
