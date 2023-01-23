feat: add option to average trials. fix bugs

scripts/fit_psd.py
@@ -13,6 +13,7 @@ from fooof import FOOOFGroup, fit_fooof_3d
 PROJECT_PATH = 'G:/Shared drives/v1_v4_1024/'
 SESSIONS = ['L_SNR_250717'] # List of sessions to analyze
 FS = 500 # sampling frequency (Hz)
+AVERAGE_TRIALS = True # average across trials before fitting
 
 # SpecParam settings
 FREQ_RANGE = [1, 40] 
@@ -42,13 +43,21 @@ def main():
             # load data
             data_in = np.load(f'{path_in}/{fname_in}')
         
-            # parameterize spectra
+            # initialize SpecParam
             fg = FOOOFGroup(**SPEC_PARAM_SETTINGS)
-            params = fit_fooof_3d(fg, data_in['freq'], data_in['spectra'], freq_range=FREQ_RANGE, n_jobs=N_JOBS)
+
+            # # parameterize spectra
+            if AVERAGE_TRIALS:
+                fg.fit(data_in['freq'], np.mean(data_in['spectra'], axis=0), \
+                    freq_range=FREQ_RANGE)
+
+            else:
+                fit_fooof_3d(fg, data_in['freq'], data_in['spectra'], \
+                    freq_range=FREQ_RANGE, n_jobs=N_JOBS)
 
             # save results
             fname_out = fname_in.replace('.npz', '.csv')
-            params.save(f"{path_out}/{fname_out}", save_results=True, save_settings=True)
+            fg.save(f"{path_out}/{fname_out}", save_results=True, save_settings=True)
 
 
 if __name__ == "__main__":