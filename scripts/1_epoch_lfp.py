2cf937f6f4425c46633ea6d24a9bfee0c678403b
Michael <mpreston@ucsd.edu>
2023-02-24 16:41:53 -0800

    chore: update epoch duration

--- scripts/1_epoch_lfp.py
+++ scripts/1_epoch_lfp.py
@@ -15,10 +15,15 @@ from utils import epoch_nix
 # settings
 PROJECT_PATH = 'G:/Shared drives/v1_v4_1024'
 SESSIONS = ['A_SNR_140819','L_SNR_250717'] # List of sessions to analyze
-FS = 500 # sampling frequency (Hz)
-IDX_ZERO = 150 # index of zero in the lfp epoch
+FS = 500 # LFP sampling frequency (Hz)
+IDX_ZERO = 150 # index of zero in the lfp epoch (stimulus onset)
+DURATION = 0.3 # duration of pre-/post-stimulus epoch (s)
 
 def main():
+    # calculate number of samples
+    n_samples = int(DURATION * FS)
+
+    # loop over sessions
     for session in SESSIONS:
         # display progress
         print(f"\nAnalyzing session: {session}")
@@ -29,14 +34,16 @@ def main():
         if not os.path.exists(path_out):
             os.makedirs(path_out)
 
-        # load and epoch data
+        # loop over files (arrays)
         files = os.listdir(path_in)
         for file in files :
             print(f"\t{file}")
+            # load and epoch data
             lfp = epoch_nix(f"{path_in}/{file}")
 
-            lfp_pre = lfp[...,:IDX_ZERO]
-            lfp_post = lfp[...,IDX_ZERO:]
+            # extract pre- and post-stimulus epochs
+            lfp_pre = lfp[..., IDX_ZERO-n_samples : IDX_ZERO]
+            lfp_post = lfp[..., IDX_ZERO : IDX_ZERO+n_samples]
 
             # save data to file
             np.save(f"{path_out}/{file.replace('.nix','_pre.npy')}", lfp_pre)
