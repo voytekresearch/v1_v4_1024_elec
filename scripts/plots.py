9df8caed348c37ac3670d15a9b4f59d6a655c631
Michael <mpreston@ucsd.edu>
2023-04-05 14:29:45 -0700

    feat: add erp plotting funcs

--- scripts/plots.py
+++ scripts/plots.py
@@ -280,3 +280,128 @@ def plot_spectra_2conditions(spectra_a, spectra_b, freq, ax=None, shade_sem=True
     # add legend
     ax.legend(labels)
 
+
+def plot_erp(erp, time, x_units='s', y_units='\u03BCV', 
+    annotate_time=0, legend_labels=None, ax=None):
+    """Plots a voltage versus time graph of an evoked response (ERP).
+
+    Parameters
+    ----------
+    erp : array_like
+        Voltage vector of the ERP.
+    time : array_like
+        Time vector of the ERP.
+    x_units : str, optional
+        Units for the x-axis (default is 's').
+    y_units : str, optional
+        Units for the y-axis (default is 'μV').
+    annotate_time : float, optional
+        Time point to annotate with a vertical line (default is 0).
+    legend_labels : list, optional
+        List of labels for the legend (default is None).
+    ax : matplotlib.axes.Axes, optional
+        Axes object to plot on (default is None).
+
+    Returns
+    -------
+    fig, ax : matplotlib.figure.Figure, matplotlib.axes.Axes
+
+    """
+
+    # create figure
+    if ax is None:
+        fig, ax = plt.subplots()
+        ax_provided = False
+    else:
+        ax_provided = True
+
+    # one ERP only
+    print(erp.shape)
+    if np.ndim(erp) == 1:
+        # plot
+        ax.plot(time, erp)
+
+    # multiple ERPs
+    elif np.ndim(erp) ==2:
+        # plot
+        ax.plot(time, erp.T)
+
+        # label
+        if not legend_labels is None:
+            ax.legend(legend_labels)
+
+    # label
+    ax.set(xlabel=f'time ({x_units})', ylabel=f'voltage ({y_units})')
+    ax.set_title('Evoked Response')
+
+    # annotate
+    if not annotate_time is None:
+        ax.axvline(annotate_time, color='k', linestyle='--')
+
+    else:
+        raise ValueError('ERP must be a 1D or 2D array')
+
+    # return
+    if ax_provided:
+        return ax
+    else:
+        return fig, ax
+
+
+def plot_event_traces(event_traces, time, annotate_time=0,
+    plot_mean=True, plot_std=True, plot_individual=False,
+    x_units='s', y_units='\u03BCV'):
+
+    """Plots event traces and related statistics.
+
+    Parameters
+    ----------
+    event_traces : array_like
+        An array of event traces
+    time : array_like
+        An array of time points corresponding to the event traces
+    annotate_time : int, optional
+        The time point at which to draw an annotation line. The default is 0
+    plot_mean : bool, optional
+        Whether or not to plot the mean trace. The default is True
+    plot_std : bool, optional
+        Whether or not to plot the standard deviation. The default is True
+    plot_individual : bool, optional
+        Whether or not to plot individual traces. The default is False
+    x_units : str, optional
+        The string to use in the x-axis label. The default is 's'
+    y_units : str, optional
+        The string to use in the y-axis label. The default is 'µV'
+
+    Returns
+    -------
+    fig, ax : matplotlib.figure.Figure, matplotlib.axes.Axes
+
+    """
+
+    # create figure
+    fig, ax = plt.subplots()
+
+    # plot individual traces
+    if plot_individual:
+        ax.plot(time, event_traces.T, color='grey', alpha=0.5)
+
+    # plot mean
+    et_mean = np.mean(event_traces, axis=0)
+    if plot_mean:
+        ax.plot(time, et_mean, color='k')
+
+    # plot standard deviation
+    if plot_std:
+        et_std = np.std(event_traces, axis=0)
+        ax.fill_between(time, et_mean-et_std, et_mean+et_std, color='grey', alpha=0.5)
+
+    # label
+    ax.set(xlabel=f'time ({x_units})', ylabel=f'voltage ({y_units})')
+    ax.set_title('Evoked Response')
+
+    # annotate
+    if not annotate_time is None:
+        ax.axvline(annotate_time, color='k', linestyle='--')
+
+    return fig, ax
