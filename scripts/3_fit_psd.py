4aeebfc2b7ba74c434e88e47781ffdfa91c97b25
andyrdar <aruizdargence@ucsd.edu>
2023-11-03 11:15:20 -0700

    implement to_df

--- scripts/3_fit_psd.py
+++ scripts/3_fit_psd.py
@@ -7,11 +7,13 @@ compute Spectral Parametrization for all files in a given folder (session)
 import os
 import numpy as np
 import pandas as pd
-from fooof import FOOOFGroup, fit_fooof_3d
+from specparam import SpectralGroupModel, fit_models_3d
 
 # Settings
 PROJECT_PATH = 'G:/Shared drives/v1_v4_1024/'
 FS = 500 # sampling frequency (Hz)
+N_ARRAYS = 16
+N_CHANS = 64
 AVERAGE_TRIALS = True # average across trials before fitting
 
 # SpecParam settings
@@ -34,6 +36,7 @@ def main():
         
     # loop through files
     files = os.listdir(path_in)
+    dfs = []
     for i_file, fname_in in enumerate(files):
         # show progress
         print(f'Processing file {i_file+1}/{len(files)}: \t{fname_in}')
@@ -42,19 +45,51 @@ def main():
         data_in = np.load(f'{path_in}/{fname_in}')
     
         # initialize SpecParam
-        fg = FOOOFGroup(**SPEC_PARAM_SETTINGS)
+        fg = SpectralGroupModel(**SPEC_PARAM_SETTINGS)
 
         # # parameterize spectra
         if AVERAGE_TRIALS:
             fg.fit(data_in['freq'], np.mean(data_in['spectra'], axis=0), \
                 freq_range=FREQ_RANGE)
         else:
-            fit_fooof_3d(fg, data_in['freq'], data_in['spectra'], \
+            fit_models_3d(fg, data_in['freq'], data_in['spectra'], \
                 freq_range=FREQ_RANGE, n_jobs=N_JOBS)
 
         # save results
         fname_out = fname_in.replace('.npz', '')
         fg.save(f"{path_out}/{fname_out}", save_results=True, save_settings=True)
+        
+        # create dataframe with all sessions combined
+        df_specparam = fg.to_df(SPEC_PARAM_SETTINGS['max_n_peaks'])
+
+        # get session IDs
+        if i_file%2==0:
+            session = fname_in.replace('_lfp_pre.npz', '')
+            epoch = 'pre'
+
+        else:
+            session = fname_in.replace('_lfp_post.npz', '')
+            epoch = 'post' 
+
+        # create a df with session data
+        data = {
+                'session'   :   np.repeat(session, N_ARRAYS*N_CHANS),
+                'channel'   :   np.concatenate([np.arange(N_CHANS)] * N_ARRAYS),
+                'chan_idx'  :   np.arange(N_ARRAYS*N_CHANS),
+                'array'     :   np.repeat(np.arange(N_ARRAYS), N_CHANS) + 1,
+                'epoch'     :   np.repeat(epoch, N_ARRAYS*N_CHANS)}
+
+        df_data = pd.DataFrame(data=data)
+        
+        df_sess = pd.concat([df_data, df_specparam], axis=1)
+
+        # add dfs to list of dfs for concating
+        dfs.append(df_sess)
+    
+    # join results DFs across sessions and save
+    dfs = pd.concat(dfs, ignore_index=True)
+    dfs.to_csv(fr'{PROJECT_PATH}/data/results/lfp_spectral_params.csv')
+
 
 
 if __name__ == "__main__":
