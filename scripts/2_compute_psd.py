c6b14da6c5b00f30fee1f40cd64c0a119725072e
Michael <mpreston@ucsd.edu>
2024-02-06 16:57:14 -0800

    average power across trials

--- scripts/2_compute_psd.py
+++ scripts/2_compute_psd.py
@@ -10,11 +10,10 @@ import os
 
 # imports - custom
 from paths import PROJECT_PATH, EXTERNAL_PATH
-from info import *
+from info import SESSIONS, FS, EPOCH_TIMES, N_JOBS, N_ARRAYS, N_CHANS
 from utils import compute_tfr
 
 # Settings
-SESSIONS = ['A_SNR_041018','A_SNR_140819','L_SNR_250717'] # List of essions to analyze
 FREQS = [4, 100, 128] # Frequency vector (start, stop, n_freqs)
 FREQ_SPACING = 'lin' # Frequency spacing ('lin' or 'log')
 
@@ -22,7 +21,7 @@ def main():
     # identify/create directories
     path_out_j = f'{EXTERNAL_PATH}/data/lfp/lfp_tfr/sessions'
     if not os.path.exists(path_out_j): os.makedirs(path_out_j)
-        
+
     # loop through sessions of interest
     for session in SESSIONS:
         # display progress
@@ -42,28 +41,29 @@ def main():
 
             # load data
             lfp = np.load(f'{path_in}/{fname_in}')
+            time = np.linspace(*EPOCH_TIMES, lfp.shape[-1], endpoint=False)
             
             # compute PSD
             tfr, freq = compute_tfr(lfp, FS, FREQS, freq_spacing=FREQ_SPACING, 
-                                        n_jobs=N_JOBS)
+                                    n_jobs=N_JOBS, output='avg_power')
 
             # save results
             fname_out = fname_in.replace('.npy', '.npz')
-            np.savez(f"{path_out}/{fname_out}", tfr=tfr, freq=freq)
+            np.savez(f"{path_out}/{fname_out}", tfr=tfr, time=time, freq=freq)
 
         # aggregate results across files (arrays) and save
         print(f"    Aggregating results across arrays...")
-        n_trials, n_channels, n_freqs, n_times = tfr.shape
-        spec_shape = [n_trials, n_channels*N_ARRAYS, n_freqs, n_times]
+        _, n_freqs, n_times = tfr.shape
+        spec_shape = [N_CHANS*N_ARRAYS, n_freqs, n_times]
         spectrogram = np.zeros(spec_shape)
         for i_array in range(N_ARRAYS):
             nsp_idx = int(np.ceil((i_array+1)/2))
             data_in = np.load(fr"{path_out}\NSP{nsp_idx}_array{i_array+1}_LFP.npz")
-            tfr_i = data_in['tfr']
-            spectrogram[:, i_array*tfr_i.shape[1]:(i_array+1)*tfr_i.shape[1]] = tfr_i
+            spectrogram[i_array*N_CHANS : (i_array+1)*N_CHANS] = data_in['tfr']
 
         # save results
-        np.savez(f"{path_out_j}/{session}_lfp.npz", spectrogram=spectrogram, freq=freq)
+        np.savez(f"{path_out_j}/{session}_lfp.npz", spectrogram=spectrogram, 
+                 time=time, freq=freq)
 
 
 if __name__ == "__main__":
