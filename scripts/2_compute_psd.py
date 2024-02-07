9fef270e91e5f16c70a9d95fe6d80b8b52abbb1f
Michael <mpreston@ucsd.edu>
2024-02-06 16:21:11 -0800

    analyze full epoch only

--- scripts/2_compute_psd.py
+++ scripts/2_compute_psd.py
@@ -7,10 +7,11 @@ Compute spectral power of LFP data using the multitaper method.
 # imports - general
 import numpy as np
 import os
-from mne.time_frequency import tfr_array_multitaper
 
 # imports - custom
+from paths import PROJECT_PATH, EXTERNAL_PATH
 from info import *
+from utils import compute_tfr
 
 # Settings
 SESSIONS = ['A_SNR_041018','A_SNR_140819','L_SNR_250717'] # List of essions to analyze
@@ -19,7 +20,7 @@ FREQ_SPACING = 'lin' # Frequency spacing ('lin' or 'log')
 
 def main():
     # identify/create directories
-    path_out_j = f'{PROJECT_PATH}/data/lfp/lfp_tfr/sessions'
+    path_out_j = f'{EXTERNAL_PATH}/data/lfp/lfp_tfr/sessions'
     if not os.path.exists(path_out_j): os.makedirs(path_out_j)
         
     # loop through sessions of interest
@@ -29,11 +30,12 @@ def main():
 
         # identify/create directories
         path_in = f'{PROJECT_PATH}/data/lfp/lfp_epochs/{session}'
-        path_out = f'{PROJECT_PATH}/data/lfp/lfp_tfr/{session}'
+        path_out = f'{EXTERNAL_PATH}/data/lfp/lfp_tfr/{session}'
         if not os.path.exists(path_out): os.makedirs(path_out)
             
         # loop through files
         files = os.listdir(path_in)
+        files = [f for f in files if not "pre" in f and not "post" in f]
         for i_file, fname_in in enumerate(files):
             # show progress
             print(f'    Processing file {i_file+1}/{len(files)}: \t{fname_in}')
@@ -63,37 +65,6 @@ def main():
 
             # save results
             np.savez(f"{path_out_j}/{session}_lfp_{epoch}.npz", spectrogram=spectrogram, freq=freq)
-
-
-def compute_tfr(lfp, fs, freqs, freq_spacing='lin', time_window_length=0.5, freq_bandwidth=4, 
-                n_jobs=-1, decim=1, verbose=False):
-    """
-    Compute time-frequency representation (TFR) of LFP data.
-
-    Parameters
-    ----------
-    lfp : 3d array
-        LFP data (trials x channels x samples).
-    fs : int
-        Sampling frequency.
-    freqs : 1d array
-        Frequency vector (start, stop, n_freqs).
-    """
-
-    # define hyperparameters
-    if freq_spacing == 'lin':
-        freq = np.linspace(*freqs)
-    elif freq_spacing == 'log':
-        freq = np.logspace(*np.log10(freqs[:2]), freqs[2])
-    n_cycles = freq * time_window_length # set n_cycles based on fixed time window length
-    time_bandwidth =  time_window_length * freq_bandwidth # must be >= 2
-
-    # TF decomposition using multitapers
-    tfr = tfr_array_multitaper(lfp, sfreq=fs, freqs=freq, n_cycles=n_cycles, 
-                                time_bandwidth=time_bandwidth, output='power', n_jobs=n_jobs,
-                                decim=decim, verbose=verbose)
-
-    return tfr, freq
 
 
 if __name__ == "__main__":
