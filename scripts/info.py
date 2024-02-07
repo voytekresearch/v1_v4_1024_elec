8f2deba783d86afd0e3b56772626e11790d87739
Michael <mpreston@ucsd.edu>
2024-02-06 16:59:53 -0800

    add session and epoch_times vars

--- scripts/info.py
+++ scripts/info.py
@@ -1,7 +1,11 @@
 """ Dataset information """
 
+# Dataset details
+SESSIONS = ['A_SNR_041018','A_SNR_140819','L_SNR_250717']
+
 # LFP data details
 FS = 500 # LFP sampling frequency (Hz)
+EPOCH_TIMES = [-0.3, 0.7] # start and end times of the LFP epoch (s)
 IDX_ZERO = 150 # index of zero in the lfp epoch (stimulus onset)
 N_ARRAYS = 16 # number of arrays per session
 N_CHANS = 64 # number of channels per array
