cb2f85a55367fcbc75d6dee0931e1d8759530ff7
Michael <mpreston@ucsd.edu>
2024-03-20 14:02:03 -0700

    fix df_data creation. clean

--- scripts/plotting/plot_spectral_time.py
+++ scripts/plotting/plot_spectral_time.py
@@ -1,5 +1,6 @@
 """
-This script plots the results of SpectralTimeModel
+This script fits the SpectralTimeModel to the LFP restults (results of
+scripts/pipeline/step2_compute_spectrogram.py)
 
 """
 
@@ -16,7 +17,7 @@ from specparam import SpectralTimeEventModel, Bands
 import sys
 sys.path.append("code")
 from paths import EXTERNAL_PATH
-from info import SESSIONS, FS, N_ARRAYS, IDX_ZERO, N_CHANS
+from info import SESSIONS, N_ARRAYS, N_CHANS
 from settings import SPECPARAM_SETTINGS, BANDS
 
 # settings
@@ -27,10 +28,6 @@ def main():
     path_in = f"{EXTERNAL_PATH}/data/lfp/lfp_tfr/sessions"
     path_out = f"{EXTERNAL_PATH}/data/lfp/SpectralTimeModel"
     if not os.path.exists(path_out): os.makedirs(path_out)
-
-    # fig_path = f'{EXTERNAL_PATH}/figures/SpectralTimeModel'
-    # if not os.path.exists(fig_path): os.makedirs(fig_path)
-
     path_results = f'{EXTERNAL_PATH}/data/results'
     if not os.path.exists(path_results): os.makedirs(path_results)
 
@@ -41,35 +38,30 @@ def main():
         print(f"\nAnalyzing session: {session}")
 
         # load data
-        fname = f"{path_in}/{session}_lfp.npz"
-        print(fname)
-        data_in = np.load(fname)
-        print(data_in.files)
+        fname = f"{session}_lfp.npz"
+        data_in = np.load(f"{path_in}/{fname}")
 
         # set variables
         tfr = data_in['spectrogram']
-        time = data_in['time']
         freq = data_in['freq']
-        print(f"Shape tfr:\t{tfr.shape}")
-        print(f"Shape time:\t{time.shape}")
-        print(f"Shape freq:\t{freq.shape}")
 
         # apply SpectralTimeModel
-        spec = tfr
         stm = SpectralTimeEventModel(**SPECPARAM_SETTINGS)
-        stm.fit(freq, spec)
-        # stm.plot(save_fig=True, file_name=f"{session}_spectra.png", file_path=fig_path)
+        stm.fit(freq, tfr)
 
         # save specparam Time Model results object
-        stm.save(f'{path_out}/{fname.replace('.npz', '')}', save_results=True, save_settings=True)
+        stm.save(f"{path_out}/{fname.replace('.npz', '')}", save_results=True, 
+                 save_settings=True)
 
         # create a dataframe for reuslts
         df_stm = stm.to_df(Bands(BANDS))
+        n_bins = tfr.shape[-1]
         df_data = pd.DataFrame({
-            'session'   :   np.repeat(session, N_ARRAYS*N_CHANS),
-            'channel'   :   np.concatenate([np.arange(N_CHANS)] * N_ARRAYS),
-            'chan_idx'  :   np.arange(N_ARRAYS*N_CHANS),
-            'array'     :   np.repeat(np.arange(N_ARRAYS), N_CHANS) + 1,})
+            'session'   :   np.repeat(session, N_ARRAYS*N_CHANS*n_bins),
+            'channel'   :   np.concatenate([np.repeat(np.arange(N_CHANS), 
+                                                      tfr.shape[-1])]*N_ARRAYS),
+            'chan_idx'  :   np.repeat(np.arange(N_ARRAYS*N_CHANS), n_bins),
+            'array'     :   np.repeat(np.arange(N_ARRAYS), N_CHANS*n_bins) + 1})
         df_sess = pd.concat([df_data, df_stm], axis=1)
 
         # add df to list
