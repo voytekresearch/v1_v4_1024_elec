f93676eb00d4a20f16993baced768e15632e5a46
Michael <mpreston@ucsd.edu>
2024-03-22 12:28:18 -0700

    add TOTAL_CHANS

--- code/info.py
+++ code/info.py
@@ -9,3 +9,4 @@ EPOCH_TIMES = [-0.3, 0.7] # start and end times of the LFP epoch (s)
 IDX_ZERO = 150 # index of zero in the lfp epoch (stimulus onset)
 N_ARRAYS = 16 # number of arrays per session
 N_CHANS = 64 # number of channels per array
+TOTAL_CHANS = N_ARRAYS * N_CHANS # total number of channels per session
