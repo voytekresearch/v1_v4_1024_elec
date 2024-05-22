1d18046040abdce760dd51b47259e14bdb856d83
Michael <mpreston@ucsd.edu>
2024-05-22 15:12:03 -0700

    update to handle new step2a output
    spectral results are no longer averaged over trials, so this must be
    performed in this script

--- scripts/pipeline/step3a_fit_spectra.py
+++ scripts/pipeline/step3a_fit_spectra.py
@@ -19,7 +19,7 @@ from specparam import SpectralGroupModel, Bands
 import sys
 sys.path.append('code')
 from paths import EXTERNAL_PATH
-from info import N_ARRAYS, N_CHANS
+from info import SESSIONS, N_ARRAYS, N_CHANS
 from settings import N_JOBS, BANDS, SPECPARAM_SETTINGS
 
 def main():
@@ -31,40 +31,41 @@ def main():
     if not os.path.exists(path_results): os.makedirs(path_results)
         
     # loop through files
-    files = os.listdir(path_in)
     dfs = []
-    for i_file, fname_in in enumerate(files):
-        f_params = fname_in.split('_')
-        session = '_'.join(f_params[:3])
-        epoch = f_params[4]
-
+    for i_session, session in enumerate(SESSIONS):
         # show progress
-        print(f'Processing file {i_file+1}/{len(files)}: \t{session}')
+        print(f'Processing file {i_session+1}/{len(SESSIONS)}: \t{session}')
 
         # load data
-        data_in = np.load(f'{path_in}/{fname_in}')
-        spectra = data_in['spectra']
+        fname_in = f"{session}_spectra.npz"
+        data = np.load(f'{path_in}/{fname_in}')
 
-        # parameterize spectra
-        fg = SpectralGroupModel(**SPECPARAM_SETTINGS)
-        fg.fit(data_in['freq'], spectra, n_jobs=N_JOBS)
+        # analyze baseline and encoding epochs
+        for spectra, epoch in zip([data['spectra_pre'], data['spectra_post']], 
+                                  ['pre', 'post']):
+            # average over trials
+            spectra = np.mean(spectra, axis=0)
 
-        # save specparam results object
-        fg.save(f"{path_out}/{fname_in.replace('.npz', '')}", save_results=True, 
-                save_settings=True, save_data=True)
-    
-        # create dataframe of results
-        df_specparam = fg.to_df(Bands(BANDS))
-        df_data = pd.DataFrame({
-            'session'   :   np.repeat(session, N_ARRAYS*N_CHANS),
-            'channel'   :   np.concatenate([np.arange(N_CHANS)] * N_ARRAYS),
-            'chan_idx'  :   np.arange(N_ARRAYS*N_CHANS),
-            'array'     :   np.repeat(np.arange(N_ARRAYS), N_CHANS) + 1,
-            'epoch'     :   np.repeat(epoch, N_ARRAYS*N_CHANS)})
-        df_sess = pd.concat([df_data, df_specparam], axis=1)
+            # parameterize spectra
+            fg = SpectralGroupModel(**SPECPARAM_SETTINGS)
+            fg.fit(data['freq'], spectra, n_jobs=N_JOBS)
 
-        # add df to list
-        dfs.append(df_sess)
+            # save specparam results object
+            fg.save(f"{path_out}/{fname_in.replace('.npz', '')}", save_results=True, 
+                    save_settings=True, save_data=True)
+        
+            # create dataframe of results
+            df_specparam = fg.to_df(Bands(BANDS))
+            df_data = pd.DataFrame({
+                'session'   :   np.repeat(session, N_ARRAYS*N_CHANS),
+                'channel'   :   np.concatenate([np.arange(N_CHANS)] * N_ARRAYS),
+                'chan_idx'  :   np.arange(N_ARRAYS*N_CHANS),
+                'array'     :   np.repeat(np.arange(N_ARRAYS), N_CHANS) + 1,
+                'epoch'     :   np.repeat(epoch, N_ARRAYS*N_CHANS)})
+            df_sess = pd.concat([df_data, df_specparam], axis=1)
+
+            # add df to list
+            dfs.append(df_sess)
     
     # join results DFs across sessions and save
     dfs = pd.concat(dfs, ignore_index=True)
