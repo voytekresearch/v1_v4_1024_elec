a6a494ade0e444c13f6d698859a4f81fd40ff222
andyrdar <aruizdargence@ucsd.edu>
2024-06-12 17:06:01 -0700

    generalize data pre-processing per folder

---
+++ V1_v4_1024_electrode_resting_state_data/signal_processing/calculate_LFP_ARD.py
@@ -0,0 +1,107 @@
+"""Calculate LFP
+
+Calculate the Local Field Potential (LFP) per channel from the raw
+analog signal.
+
+Usage:
+    calculate_LFP.py --ns6=E:/V1_v4_1024_electrode_resting_state_data/data/A_SNR_140819/raw --out=FOLDER --odml=FILE --array=INT
+    calculate_LFP.py --ns6=E:/V1_v4_1024_electrode_resting_state_data/data/A_SNR_140819/raw --out=E:/V1_v4_1024_electrode_resting_state_data/data/A_SNR_140819/LFP_ARD --odml=E:/V1_v4_1024_electrode_resting_state_data/data/A_SNR_140819/metadata_A_SNR_140819.odml --array=2 --eyesig=FILE
+
+Options:
+    -h --help     Show this screen and terminate script.
+    --ns6=FOLDER  Path to folder containing all .ns6 data files.
+    --odml=FILE   Path to .odml metadata file.
+    --array=INT   Number of the analized array.
+    --eyesig=FILE Path to the eye signals
+    --out=FOLDER    Output folder path.
+"""
+from docopt import docopt
+from utils import anasig_from_array, merge_anasiglist, mark_epochs
+import quantities as pq
+import neo
+from neo.io.nixio import NixIO
+import gc
+import warnings
+import scipy
+
+import os
+import glob
+
+if __name__ == '__main__':
+    # Get arguments
+    vargs = docopt(__doc__)
+    folder_ns6 = vargs['--ns6']
+    array_id = vargs['--array']
+    odmlpath = vargs['--odml']
+    folder_out = vargs['--out']
+    try:
+        path_eye = vargs['--eyesig']
+    except KeyError:
+        path_eye = None
+        warnings.warn('No eyesignals found.')
+
+    # call all .ns6 files in folder
+    ns6_files = glob.glob(os.path.join(folder_ns6, '*.ns6'))
+
+    # loop through arrays
+    N_ARRAYS=16
+    for i_array in range(1, N_ARRAYS+1):
+        print(f"    plotting array: {i_array}/{N_ARRAYS}")
+        idx_nsp = (i_array+1) // 2
+
+        path_lfp = os.path.join(folder_out, os.path.basename(path_ns6).replace('.ns6', '.nix'))
+
+    # counter for arrays
+    count = 1
+    for path_ns6 in ns6_files:
+
+            path_lfp = os.path.join(folder_out, os.path.basename(path_ns6).replace('.ns6', '.nix'))
+
+            print(f"    current file: {path_ns6}")
+
+            lfp = []
+            # 1. Get the analogsignal of each index (i.e. electrode)
+            for anasig in anasig_from_array(path_ns6, array_id, odmlpath=odmlpath):
+
+                ## 2. Filter the signal between 1Hz and 150Hz
+                #anasig = butter(anasig, lowpass_freq=150.0*pq.Hz)
+                #gc.collect()
+
+                # 3. Downsample signal from 30kHz to 500Hz resolution (factor 60)
+                anasig = anasig.downsample(60, ftype='fir')
+                gc.collect()
+
+                ## 4. Bandstop filter the 50, 100 and 150 Hz frequencies
+                ## Compensates for artifacts from the European electric grid
+                #for fq in [50, 100, 150]:
+                #    anasig = butter(anasig,
+                #                    highpass_freq=(fq + 2)*pq.Hz,
+                #                    lowpass_freq=(fq - 2)*pq.Hz)
+                #    gc.collect()
+
+                lfp.append(anasig)
+
+
+        # Use custom function to merge analogsignals
+        print(f"lfp list content: {lfp.shape}")
+        lfp = merge_anasiglist(lfp)
+
+        # Create empty neo blocks for new datafiles
+        array_block = neo.Block(name='LFP signal from ' + path_ns6)
+        array_block.segments = [neo.Segment()]
+
+        # Enrich with analog signal data
+        array_block.segments[0].analogsignals = []
+        array_block.segments[0].analogsignals.append(lfp)
+
+        # Mark epochs, based on metadata
+        array_block = mark_epochs(array_block, odmlpath, eyepath=path_eye)
+
+        # Save to nix
+        outFile = NixIO(path_lfp, mode='ow')
+        outFile.write(array_block)
+        outFile.close()
+
+        # Save to mat
+        matdict = {'lfp': lfp.magnitude, **lfp.array_annotations}
+        scipy.io.savemat(path_lfp.replace('nix', 'mat'), matdict)
