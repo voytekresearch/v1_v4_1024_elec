12269080c0dbc14a07c8f5353091fab35f3e8ae1
Michael <mpreston@ucsd.edu>
2024-07-29 15:09:02 -0700

    update comments

--- V1_v4_1024_electrode_resting_state_data/signal_processing/calculate_LFP_ARD.py
+++ V1_v4_1024_electrode_resting_state_data/signal_processing/calculate_LFP_ARD.py
@@ -1,19 +1,12 @@
 """Calculate LFP
 
-Calculate the Local Field Potential (LFP) per channel from the raw
-analog signal.
+Calculate the Local Field Potential (LFP) per channel from the raw analog 
+signal. The LFP is downsampled to 500 Hz and saved as a .nix and .mat file.
+The filtering steps employed by Chen et al. 2022 have been removed.
 
-Usage:
-    python calculate_LFP_ARD.py 
-
-Options:
-    -h --help     Show this screen and terminate script.
-    --ns6=FOLDER  Path to folder containing all .ns6 data files.
-    --odml=FILE   Path to .odml metadata file.
-    --array=INT   Number of the analized array.
-    --eyesig=FILE Path to the eye signals
-    --out=FOLDER    Output folder path.
 """
+
+# imports - general
 from utils import anasig_from_array, merge_anasiglist, mark_epochs
 import neo
 from neo.io.nixio import NixIO
@@ -22,19 +15,22 @@ import scipy
 import os
 import glob
 
+# imports - custom
 import sys
 sys.path.append('code')
 from paths import EXTERNAL_PATH
 from info import SESSIONS, N_ARRAYS
 
-if __name__ == '__main__':
+# settings 
+SESSIONS = ['A_SNR_140819','L_SNR_250717']
 
-    SESSIONS = ['A_SNR_140819','L_SNR_250717']
+
+if __name__ == '__main__':
 
     # loop through sessions of interest
     for session in SESSIONS:
 
-        # Get arguments
+        # set/create directories
         folder_ns6 = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/{session}/raw"
         odmlpath = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/{session}/metadata_{session}.odml "
         folder_out = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/{session}/LFP_unfiltered"
@@ -45,16 +41,15 @@ if __name__ == '__main__':
 
         # loop through arrays
         for i_array in range(1, N_ARRAYS+1):
-            print(f"    plotting array: {i_array}/{N_ARRAYS}")
+            print(f"    pre-processing array: {i_array}/{N_ARRAYS}")
 
-            idx_nsp = (i_array-1) // 2                                         # retrieve ns6 file per array
+            # get paths
+            idx_nsp = (i_array-1) // 2  # retrieve ns6 file per array
             path_ns6 = ns6_files[idx_nsp]
-
-            path_lfp = folder_out + f'/NSP{idx_nsp+1}_array{i_array}_LFP.nix'      # define path outh
-
+            path_lfp = folder_out + f'/NSP{idx_nsp+1}_array{i_array}_LFP.nix' 
         
-            lfp = []
             # 1. Get the analogsignal of each index (i.e. electrode)
+            lfp = []
             for anasig in anasig_from_array(path_ns6, i_array, odmlpath=odmlpath):
 
                 # Downsample signal from 30kHz to 500Hz resolution (factor 60)
