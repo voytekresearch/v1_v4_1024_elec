0c57f63c85d92b1e3bf1708366ebb83ac44a1057
andyrdar <aruizdargence@ucsd.edu>
2024-07-29 15:00:04 -0700

    include all session to lfp preprocessing

--- V1_v4_1024_electrode_resting_state_data/signal_processing/calculate_LFP_ARD.py
+++ V1_v4_1024_electrode_resting_state_data/signal_processing/calculate_LFP_ARD.py
@@ -26,78 +26,86 @@ import scipy
 import os
 import glob
 
+import sys
+sys.path.append('code')
+from paths import EXTERNAL_PATH
+from info import SESSIONS, N_ARRAYS
+
 if __name__ == '__main__':
-    # set external drive path
-    EXTERNAL_PATH = 'E:'
 
-    # Get arguments
-    vargs = docopt(__doc__)
-    folder_ns6 = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/A_SNR_140819/raw" 
-    #array_id = vargs['--array']
-    odmlpath = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/A_SNR_140819/metadata_A_SNR_140819.odml "
-    folder_out = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/A_SNR_140819/LFP_ARD"
-    # try:
-    #     path_eye = vargs['--eyesig']
-    # except KeyError:
-    #     path_eye = None
-    #     warnings.warn('No eyesignals found.')
+    SESSIONS = ['A_SNR_140819','L_SNR_250717']
 
-    # call all .ns6 files in folder
-    ns6_files = glob.glob(os.path.join(folder_ns6, '*.ns6'))
+    # loop through sessions of interest
+    for session in SESSIONS:
 
-    # loop through arrays
-    N_ARRAYS=16
-    for i_array in range(1, N_ARRAYS+1):
-        print(f"    plotting array: {i_array}/{N_ARRAYS}")
+        # Get arguments
+        folder_ns6 = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/{session}/raw"
+        odmlpath = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/{session}/metadata_A_SNR_140819.odml "
+        folder_out = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/{session}/LFP_unfiltered"
+        if not os.path.exists(folder_out): os.makedirs(folder_out)
+        vargs = docopt(__doc__)
+         #array_id = vargs['--array']
+        # try:
+        #     path_eye = vargs['--eyesig']
+        # except KeyError:
+        #     path_eye = None
+        #     warnings.warn('No eyesignals found.')
 
-        idx_nsp = (i_array-1) // 2                                         # retrieve ns6 file per array
-        path_ns6 = ns6_files[idx_nsp]
+        # call all .ns6 files in folder
+        ns6_files = glob.glob(os.path.join(folder_ns6, '*.ns6'))
 
-        path_lfp = folder_out + f'/NSP{idx_nsp+1}_array{i_array}_LFP.nix'      # define path outh
+        # loop through arrays
+        for i_array in range(1, N_ARRAYS+1):
+            print(f"    plotting array: {i_array}/{N_ARRAYS}")
 
-      
-        lfp = []
-        # 1. Get the analogsignal of each index (i.e. electrode)
-        for anasig in anasig_from_array(path_ns6, i_array, odmlpath=odmlpath):
+            idx_nsp = (i_array-1) // 2                                         # retrieve ns6 file per array
+            path_ns6 = ns6_files[idx_nsp]
 
-            ## 2. Filter the signal between 1Hz and 150Hz
-            #anasig = butter(anasig, lowpass_freq=150.0*pq.Hz)
-            #gc.collect()
+            path_lfp = folder_out + f'/NSP{idx_nsp+1}_array{i_array}_LFP.nix'      # define path outh
 
-            # 3. Downsample signal from 30kHz to 500Hz resolution (factor 60)
-            anasig = anasig.downsample(60, ftype='fir')
-            gc.collect()
+        
+            lfp = []
+            # 1. Get the analogsignal of each index (i.e. electrode)
+            for anasig in anasig_from_array(path_ns6, i_array, odmlpath=odmlpath):
 
-            ## 4. Bandstop filter the 50, 100 and 150 Hz frequencies
-            ## Compensates for artifacts from the European electric grid
-            #for fq in [50, 100, 150]:
-            #    anasig = butter(anasig,
-            #                    highpass_freq=(fq + 2)*pq.Hz,
-            #                    lowpass_freq=(fq - 2)*pq.Hz)
-            #    gc.collect()
+                ## 2. Filter the signal between 1Hz and 150Hz
+                #anasig = butter(anasig, lowpass_freq=150.0*pq.Hz)
+                #gc.collect()
 
-            lfp.append(anasig)
+                # 3. Downsample signal from 30kHz to 500Hz resolution (factor 60)
+                anasig = anasig.downsample(60, ftype='fir')
+                gc.collect()
+
+                ## 4. Bandstop filter the 50, 100 and 150 Hz frequencies
+                ## Compensates for artifacts from the European electric grid
+                #for fq in [50, 100, 150]:
+                #    anasig = butter(anasig,
+                #                    highpass_freq=(fq + 2)*pq.Hz,
+                #                    lowpass_freq=(fq - 2)*pq.Hz)
+                #    gc.collect()
+
+                lfp.append(anasig)
 
 
-        # Use custom function to merge analogsignals
-        lfp = merge_anasiglist(lfp)
+            # Use custom function to merge analogsignals
+            lfp = merge_anasiglist(lfp)
 
-        # Create empty neo blocks for new datafiles
-        array_block = neo.Block(name='LFP signal from ' + path_ns6)
-        array_block.segments = [neo.Segment()]
+            # Create empty neo blocks for new datafiles
+            array_block = neo.Block(name='LFP signal from ' + path_ns6)
+            array_block.segments = [neo.Segment()]
 
-        # Enrich with analog signal data
-        array_block.segments[0].analogsignals = []
-        array_block.segments[0].analogsignals.append(lfp)
+            # Enrich with analog signal data
+            array_block.segments[0].analogsignals = []
+            array_block.segments[0].analogsignals.append(lfp)
 
-        # Mark epochs, based on metadata
-        array_block = mark_epochs(array_block, odmlpath, eyepath=None)
+            # Mark epochs, based on metadata
+            array_block = mark_epochs(array_block, odmlpath, eyepath=None)
 
-        # Save to nix
-        outFile = NixIO(path_lfp, mode='ow')
-        outFile.write(array_block)
-        outFile.close()
+            # Save to nix
+            outFile = NixIO(path_lfp, mode='ow')
+            outFile.write(array_block)
+            outFile.close()
 
-        # Save to mat
-        matdict = {'lfp': lfp.magnitude, **lfp.array_annotations}
-        scipy.io.savemat(path_lfp.replace('nix', 'mat'), matdict)
+            # Save to mat
+            matdict = {'lfp': lfp.magnitude, **lfp.array_annotations}
+            scipy.io.savemat(path_lfp.replace('nix', 'mat'), matdict)
