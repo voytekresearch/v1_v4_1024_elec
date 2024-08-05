38e317dcf214519d8c53c6d644ed63d16e5ebfed
Michael <mpreston@ucsd.edu>
2024-08-05 16:13:04 -0700

    upgrade max_n_peaks

--- code/settings.py
+++ code/settings.py
@@ -12,7 +12,7 @@ N_JOBS = -1 # number of jobs for parallel processing
 SPECPARAM_SETTINGS = {
     'peak_width_limits' :   [2, 12],    # (default: (0.5, 12.0)) should be >= 2*frequency_resolution
     'min_peak_height'   :   0.1,        # (default: 0.0)
-    'max_n_peaks'       :   3,          # (default: inf)
+    'max_n_peaks'       :   4,          # (default: inf)
     'peak_threshold'    :   2.0,        # (default: 2.0)
     'aperiodic_mode'    :   'knee',     # (default: 'fixed')
     'verbose'           :   False}      # (default: True)
