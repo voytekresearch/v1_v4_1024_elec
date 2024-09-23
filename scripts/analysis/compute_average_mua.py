fcf9c12ef122af0880a6178c77ad5f3509ce0e92
Michael <mpreston@ucsd.edu>
2024-09-23 14:54:00 -0700

    refactor

--- scripts/analysis/compute_average_mua.py
+++ scripts/analysis/compute_average_mua.py
@@ -4,7 +4,6 @@ Compute average MUA for pre- and post-stimulus epochs, across all sessions.
 """
 
 # imports - standard
-import os
 import numpy as np
 import pandas as pd
 from neurodsp.utils import create_times
@@ -19,57 +18,57 @@ from info import FS, SESSIONS, N_ARRAYS, N_CHANS, EPOCH_TIMES
 
 def main():
     # init
-    dfs =[]
+    df_list =[]
 
-    # loop through epochs
+    # loop through sessions and arrays
     for session in SESSIONS:
         for i_array in range(N_ARRAYS):
+            # get current filename
             nps_idx = (i_array+2) // 2
+            fname_in = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/{session}/MUAe/NSP{nps_idx}_array{i_array+1}_MUAe.nix"
 
             # display progress
-            fname_in = f"{EXTERNAL_PATH}/V1_v4_1024_electrode_resting_state_data/data/{session}/MUAe/NSP{nps_idx}_array{i_array+1}_MUAe.nix"
             print(f"\nProcessing session {session}:")
             print(f"    current file: {fname_in.split('/')[-1]}")
             
-            # create dataframe for session data
+            # initialize data dictionary
             data = {
                 'session'   :   np.repeat(session, N_CHANS),
                 'array'     :   np.repeat(i_array, N_CHANS) + 1,
                 'channel'   :   np.concatenate([np.arange(N_CHANS)])}
             
+            # load data
             segment, _ = load_nix(fname_in)
 
             # epoch data into trials around stimulus times
             epochs = epoch_neo_segment(segment, segment.epochs[0], reset_time=True) 
-            epoch_times = create_times(epochs.shape[-1]/FS, FS, 
+            time = create_times(epochs.shape[-1]/FS, FS, 
                                        start_val=EPOCH_TIMES[0])
 
             # average MUA over trials
             mua_mean = np.mean(epochs, axis=0) # channels x time
             
             # find indices for pre- and post-stimulus epochs
-            idx_pre = np.logical_and(epoch_times>=EPOCH_TIMES[0], epoch_times<0)
-            idx_post = np.logical_and(epoch_times>=0, epoch_times<EPOCH_TIMES[1]) 
+            idx_pre = np.logical_and(time>=EPOCH_TIMES[0], time<0)
+            idx_post = np.logical_and(time>=0, time<EPOCH_TIMES[1]) 
 
             # loop through epochs 
             for epoch, indices in zip(['pre', 'post'], [idx_pre, idx_post]):
-                df_ar = pd.DataFrame(data=data)
-                df_ar['epoch'] = epoch
-
-                # index time dimension
-                mua_array = mua_mean[:, indices]
 
                 # take average over time window
-                df_ar[f'mua'] = np.mean(mua_array, axis=1)
+                mua_epoch = np.mean(mua_mean[:, indices], axis=1)
 
-                # add results to list
-                dfs.append(df_ar)
+                # store results
+                df = pd.DataFrame(data=data)
+                df['epoch'] = epoch
+                df[f'mua'] = np.mean(mua_epoch)
+                df_list.append(df)
 
     # concatenate results into single dataframe
-    df = pd.concat(dfs, ignore_index=True)
+    df = pd.concat(df_list, ignore_index=True)
 
     # save results
-    df.to_csv(f'{EXTERNAL_PATH}/data/results/mua_df.csv')
+    df.to_csv(f'{EXTERNAL_PATH}/data/results/mua_df.csv', index=False)
 
 
 if __name__ == '__main__':
